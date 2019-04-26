003d8c55280a0c178815edcf374001bf036494d8
Alexis Louie <alouie1020@gmail.com>
2019-04-25 22:13:35 -0400

    Initial Commit
    Previous repo contained both the API and client. That repo has now been deleted. The API and client will now exist as two separate repos.

---
+++ routes/busyHours.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const busy_hours = require('busy-hours');
+
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+const PLACES_API_KEY = process.env.PLACES_API_KEY;
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+router.get('/', jsonParser, (req, res) => {
+    const { placeId } = req.body;
+
+    busy_hours(placeId, PLACES_API_KEY)
+        .then(data => {
+            console.log('data: ', data);
+        })
+})
+
+
+module.exports = router;
---
+++ routes/places.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PLACES_API_KEY = process.env.PLACES_API_KEY;
+
+const googleMapsClient = require('@google/maps').createClient({
+    Promise: Promise,
+    key: PLACES_API_KEY
+});
+
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+router.get('/', jsonParser, (req, res) => {
+    const { query, location } = req.query;
+    const radius = parseInt(req.query.radius);
+
+    googleMapsClient.geocode({
+        address: location
+    })
+        .asPromise()
+        .then(res => { return res.json.results })
+        .then(results => {
+            const latlng = results[0].geometry.location;
+            const coordinatesAsArray = Object.values(latlng);
+            return coordinatesAsArray;
+        })
+        .then(coordinatesAsArray => {
+            googleMapsClient.places({
+                query: query,
+                location: coordinatesAsArray,
+                radius: radius
+            })
+                .asPromise()
+                .then(res => {
+                    console.log('res received');
+                    if (res.status === 200) {
+                        return res;
+                    }
+                    else {
+                        throw Error(`Request rejected with status ${res.status}`);
+                    }
+                })
+                .catch(console.error)
+                .then(data => res.status(200).json(data))
+        })
+})
+
+
+module.exports = router;
